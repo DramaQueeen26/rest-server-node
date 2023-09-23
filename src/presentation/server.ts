1f254d2f0b9cb8a21d2f734dde38f72d01dd5285
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-09-23 15:52:40 -0400

    Variables de entorno

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,19 +1,42 @@
 import express from 'express'
+import path from 'path'
+
+interface Options {
+  port: number,
+  public_path: string
+}
 
 export class Server {
 
   private app = express()
+  private readonly port: number
+  private readonly publicPath: string
+
+  constructor( options: Options ) {
+    const { port, public_path } = options
+
+    this.port = port
+    this.publicPath = public_path
+
+  }
 
   async start() {
 
-    this.app.listen( 3000, () => {
+    this.app.listen( this.port, () => {
 
       //* Middlewares
 
       //* Public folder
-      this.app.use( express.static('public') )
+      this.app.use( express.static( this.publicPath ) )
 
-      console.log('Server running on port 3000')
+      this.app.get('*', (req, res) => {
+        
+        const indexPath = path.join(__dirname + `../../../${ this.publicPath }/index.html`)
+        return res.sendFile( indexPath )
+
+      })
+
+      console.log(`Server running on port ${ this.port }`)
 
     })
 
