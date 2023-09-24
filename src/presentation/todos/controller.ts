a94f97769e2d9ffc2eb10b8f44b7e5300251a692
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-09-24 15:53:08 -0400

    CRUD - Read

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,12 +1,32 @@
 import { Request, Response } from "express"
 
+const todos = [
+  {id: 1, text: 'buy milk', createdAt: new Date()},
+  {id: 2, text: 'buy bread', createdAt: new Date()},
+  {id: 3, text: 'buy butter', createdAt: new Date()}
+]
+
 export class TodosController {
 
   constructor() {}
 
   public getTodos = (req: Request, res: Response) => {
 
-    res.json('hola')
+    return res.json(todos)
+
+  }
+
+  public getTodoById = (req: Request, res: Response) => {
+
+    const id = +req.params.id
+
+    if( isNaN(id) ) return res.status(400).json({ error: `ID argument is not a number` })
+
+    const todo = todos.find( todo => todo.id === id);
+
+    (todo)
+    ? res.json(todo)
+    : res.status(404).json({ error: `Todo with id ${ id } not found` })
 
   }
 
