77397677af4c10a9a3049413b74217c049ec858e
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-09-27 15:32:55 -0400

    Obtener todos

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -11,7 +11,9 @@ export class TodosController {
 
   constructor() {}
 
-  public getTodos = (req: Request, res: Response) => {
+  public getTodos = async (req: Request, res: Response) => {
+
+    const todos = await prisma.todo.findMany()
 
     return res.json(todos)
 
